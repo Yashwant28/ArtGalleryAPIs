501e974c5f43a311f138fe9b1fc7259948dd3a5d
Yashwant Jangid <jangid.yashwant28@gmail.com>
2021-04-05 01:18:19 +0530

    Update Application.java
    adding test commit

--- src/main/java/com/art/gallery/Application.java
+++ src/main/java/com/art/gallery/Application.java
@@ -4,6 +4,7 @@ package com.art.gallery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
