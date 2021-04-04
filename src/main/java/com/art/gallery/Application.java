63bf5075d1fcddb8603c190e9dee97913a27c92e
Yashwant28 <jangid.yashwant28@gmail.com>
2021-04-05 01:14:09 +0530

    Adding pom.xml

---
+++ src/main/java/com/art/gallery/Application.java
@@ -0,0 +1,12 @@
+package com.art.gallery;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
